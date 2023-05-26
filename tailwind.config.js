296d1355cf5444725a99e062a98358f09465a79a
Byron Burnett <byron.burnett@outlook.com>
2023-05-26 14:49:57 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+// some customization here, omitting
+    },
+  },
+  plugins: [],
+}
